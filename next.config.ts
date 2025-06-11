2675fe940eb3b23d8be5f7ee966aaa9577589a13
OLuciani <lucianioscar1@gmail.com>
2025-06-11 18:15:41 -0300

    chore(config): configure bundle analyzer in next.config.ts
    - Added settings in next.config.ts to enable Next.js bundle analyzer
    - Updated package.json and package-lock.json to include next-bundle-analyzer dependency

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,4 @@
-
 /* import type { NextConfig } from "next";
-
-//const isDev = process.env.NODE_ENV === "development";
-const isProduction = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'res.cloudinary.com',
-    },
-  ],
-},
-async rewrites() {
-    const backendUrl = isProduction
-      ? process.env.NEXT_PUBLIC_API_URL // Asume que tienes esta variable para producción
-      : process.env.NEXT_PUBLIC_API_URL_LOCAL;
-    return [
-      {
-        source: '/api-proxy/:path*', // Cualquier ruta que comience con /api-proxy/
-        destination: `${backendUrl}/:path*`,
-      },
-    ];
-  },
-
-
-};
-
-export default nextConfig; */
-
-
-
-import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
@@ -56,4 +22,35 @@ async rewrites() {
 
 };
 
-export default nextConfig;
+export default nextConfig; */
+
+
+
+import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const withAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true', // solo lo activa si seteás esa variable
+});
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+  async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_API_URL;
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: `${backendUrl}/:path*`,
+      },
+    ];
+  },
+};
+
+export default withAnalyzer(nextConfig);
