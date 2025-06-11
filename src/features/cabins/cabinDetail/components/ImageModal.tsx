641ac3366035ea1a6155f14372754fea12a82929
OLuciani <lucianioscar1@gmail.com>
2025-06-11 16:33:03 -0300

    feat(ui): corrección en modal: src/features/cabins/cabinDetail/components/ImageModal.tsx

--- src/features/cabins/cabinDetail/components/ImageModal.tsx
+++ src/features/cabins/cabinDetail/components/ImageModal.tsx
@@ -15,7 +15,7 @@ const ImageModal: React.FC<CabinDetailProps> = ({
     <>
       {isModalOpen && (
         <div className="fixed inset-y-[-30px] inset-x-0 bg-black bg-opacity-70 z-50 flex items-center justify-center px-2 sm:px-6">
-          <div className="relative bg-white p-6 sm:p-14 lg:p-12 rounded-2xl w-full max-w-[95vw] xs:max-w-[85vw] xl:max-w-[75vw] flex items-center justify-center overflow-hidden">
+          <div className="relative bg-white p-6 sm:p-14 lg:p-12 rounded-2xl w-full max-w-[95vw] xxs:max-w-[85vw] lg:max-w-[75vw] xl:max-w-[75vw] flex items-center justify-center overflow-hidden">
             {/* Botón de cerrar */}
             <button
               onClick={closeModal}
@@ -36,7 +36,7 @@ const ImageModal: React.FC<CabinDetailProps> = ({
 
             {/* Imagen */}
             {typeof currentImageIndex === "number" && images?.[currentImageIndex] && (
-              <div className="relative w-full max-w-[95vw] xs:max-w-[85vw] xl:max-w-[65vw] max-h-[70vh] xl:max-h-[80vh] aspect-video rounded-xl overflow-hidden">
+              <div className="relative w-full max-w-[95vw] xxs:max-w-[85vw] xl:max-w-[65vw] max-h-[70vh] xl:max-h-[80vh] aspect-video rounded-xl overflow-hidden">
                 <Image
                   src={images[currentImageIndex]}
                   alt={`Imagen ${currentImageIndex + 1}`}
