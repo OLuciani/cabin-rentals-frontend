3d668b16d1c63c07431f24eac61e5a4ae6fd3e94
OLuciani <lucianioscar1@gmail.com>
2025-06-15 21:36:22 -0300

    feat(ui): ajustar textos en src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         </div>
 
         <div className="absolute inset-0 bg-opacity-40 flex flex-col items-center justify-end pb-64 md:pb-6 lg:pb-6 ">
-          <div className="text-center text-white/90 flex flex-col items-center justify-center md:gap-36   ">
+          <div className="text-center text-white/90 flex flex-col items-center justify-center md:gap-28 xxxl:gap-36  ">
             <h2 className="text-3xl sm:text-4xl md:text-5xl font-semibold mb-4">
               Escapá a la naturaleza
             </h2>
